b3a11ed770e7df0d6b1e626577468a7445c282aa
Nes224 <Nes224@hotmail.com>
2025-11-28 16:32:27 +0700

    init project 1.2

---
+++ golang_backend/internal/migration/000001_init_schema.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS "checklist_monthly";
+DROP TABLE IF EXISTS "historical_candles";
+DROP TABLE IF EXISTS "journal_entries";
+DROP TABLE IF EXISTS "market_data";
---
+++ golang_backend/internal/migration/000001_init_schema.up.sql
@@ -0,0 +1,52 @@
+CREATE TABLE "checklist_monthly" (
+    "id" bigserial PRIMARY KEY,
+    "month" varchar,
+    "item_name" varchar NOT NULL,
+    "count" integer NOT NULL,
+    "created_datetime" timestamp,
+    "updated_datetime" timestamp NULL
+);
+
+CREATE TABLE "historical_candles" (
+    "id" bigserial PRIMARY KEY,
+    "symbol" varchar NOT NULL,
+    "timeframe" varchar NOT NULL,
+    "timestamp" timestamp NOT NULL,
+    "open" double precision NOT NULL,
+    "close" double precision NOT NULL,
+    "high" double precision NOT NULL,
+    "low" double precision NOT NULL,
+    "volume" double precision NOT NULL,
+    "created_datetime" timestamp,
+    "updated_datetime" timestamp NULL
+);
+
+CREATE TABLE "journal_entries" (
+    "id" bigserial PRIMARY KEY,
+    "symbol" varchar NOT NULL,
+    "trade_1" double precision NOT NULL,
+    "trade_2" double precision NOT NULL,
+    "trade_3" double precision NOT NULL,
+    "deposit" double precision NOT NULL,
+    "withdraw" double precision NOT NULL,
+    "note" varchar,
+    "profit" double precision NOT NULL,
+    "total" double precision NOT NULL,
+    "capital" double precision NOT NULL,
+    "winrate" double precision NOT NULL,
+    "created_datetime" timestamp,
+    "updated_datetime" timestamp NULL
+);
+
+CREATE TABLE "market_data" (
+    "id" bigserial PRIMARY KEY,
+    "symbol" varchar NOT NULL,
+    "timeframe" varchar NOT NULL,
+    "timestamp" timestamp NOT NULL,
+    "open" double precision NOT NULL,
+    "close" double precision NOT NULL,
+    "high" double precision NOT NULL,
+    "low" double precision NOT NULL,
+    "volume" double precision NOT NULL,
+    "created_datetime" timestamp
+);
